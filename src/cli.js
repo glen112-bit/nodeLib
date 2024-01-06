fe8b5992a480a8fac2668cc6791b3c0b15ddfeb7
Glen112 <escobar.glen@gmail.com>
2024-01-06 00:59:34 -0300

    validaciones

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import chalk from "chalk";
 import fs from 'fs';
 import takeFile from "./index.js";
@@ -5,12 +6,12 @@ import listaValidada from "./http.validacion.js";
 
 const camino = process.argv;
 
-function imprimeLista(valida, resultado, identificador = ''){
+async function imprimeLista(valida, resultado, identificador = ''){
 if (valida){
   console.log(
     chalk.yellow('lista validada'),
     chalk.black.bgGreen(identificador),
-    listaValidada(resultado)
+    await listaValidada(resultado)
   )
 }else{
   console.log(
@@ -28,7 +29,6 @@ async function textProcess(argumentos){
   } catch(error){
     if(error.code === 'ENOENT'){
       console.log(chalk.red('archivo o direcctorio no existe'))
-      return
     }
 
   }
--- src/http.validacion.js
+++ src/http.validacion.js
@@ -1,6 +1,40 @@
 function extraeLinks(arrLinks){
   return arrLinks.map((objLink) => Object.values(objLink).join())
 }
-export default function listaValidada(listaDeLinks){
-  return extraeLinks( listaDeLinks )
+
+async function checkStatus(listaURLs){
+  const arrStatus = await Promise.all(
+   listaURLs.map(async (url) => {
+     try{
+      const response = await fetch(url);
+      return response.status 
+     }catch(error){
+      return manejaErrores(error)
+      }
+    })
+  )
+  return arrStatus
 }
+
+function manejaErrores(error){
+  if(error.cause.code === 'ENOTFOUND'){
+    return 'link no encontrado'
+  }else{
+    return 'algun error'
+  }
+}
+
+export default async function listaValidada(listaDeLinks){
+  const links =  extraeLinks( listaDeLinks );
+  const status = await checkStatus(links);
+  return listaDeLinks.map((link, indice) => ({
+    ...link,
+    status: status[indice]
+  }));
+}
+
+// async function logMovies() {
+  // const response = await fetch("http://example.com/movies.json");
+  // const movies = await response.json();
+  // console.log(movies);
+// }
